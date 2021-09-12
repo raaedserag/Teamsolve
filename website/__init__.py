5b4db2a32dba8e6788cf2e8c435ac14fa1d5e757
m-agour <mo.aggour@gmail.com>
2021-09-12 07:48:11 +0200

    Changed Json file dir to be inside Assets

--- website/__init__.py
+++ website/__init__.py
@@ -8,24 +8,6 @@ from flask_login import LoginManager
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
-
-def test():
-    from .models import User, Team, Problem
-    user1 = User(name="demon", password="hello1234e", email="dodico@com")
-    user2 = User(name="demon2",password="hello1234",  email="doddico@com")
-    user3 = User(name="demon3",password="hello1234",  email="dodfico@com")
-    db.session.add(user1)
-    db.session.add(user2)
-    db.session.add(user3)
-    db.session.commit()
-    user1.solutions.append(Problem.query.get(1))
-    user2.solutions.append(Problem.query.get(1))
-    user2.solutions.append(Problem.query.get(2))
-    user3.solutions.append(Problem.query.get(2))
-    user2.solutions.append(Problem.query.get(3))
-    db.session.commit()
-
-
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
@@ -65,9 +47,24 @@ def create_database(app):
 
 def load_problems(app):
     from website.models import Problem
-    problems = json.load(open("problems.json", "r"))
+    problems = json.load(open("assets/problems.json", "r"))
     for i in range(len(problems)):
         prob = Problem(name=problems[i])
         db.session.add(prob)
     db.session.commit()
 
+def test():
+    from .models import User, Team, Problem
+    user1 = User(name="demon", password="hello1234e", email="dodico@com")
+    user2 = User(name="demon2",password="hello1234",  email="doddico@com")
+    user3 = User(name="demon3",password="hello1234",  email="dodfico@com")
+    db.session.add(user1)
+    db.session.add(user2)
+    db.session.add(user3)
+    db.session.commit()
+    user1.solutions.append(Problem.query.get(1))
+    user2.solutions.append(Problem.query.get(1))
+    user2.solutions.append(Problem.query.get(2))
+    user3.solutions.append(Problem.query.get(2))
+    user2.solutions.append(Problem.query.get(3))
+    db.session.commit()
